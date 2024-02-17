5c1139eade810cb43169339bbc35469ef5cd7d5a
hiromuota166 <otahiromu166@gmail.com>
2024-02-17 10:07:05 +0900

    cursor勝手に変更検知されない？

--- firstproject/myapp/views.py
+++ firstproject/myapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import YourModel
+from .serializers import YourModelSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def get_your_model(request):
+    if request.method == 'GET':
+        items = YourModel.objects.all()
+        serializer = YourModelSerializer(items, many=True)
+        return Response(serializer.data)
